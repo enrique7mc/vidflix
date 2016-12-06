e9cc80fad9b180d6fad34c2c7020f04258d4b474
enrique.munguia <enrique.munguia@hulu.com>
2016-12-06 09:58:36 -0600

    add code spliting with AsyncRoute component

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   devtool: 'source-map',
   output: {
     path: path.join(__dirname, '/public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/public/'
   },
   devServer: {
     publicPath: '/public/',
